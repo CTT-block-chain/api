bcf7213c1be96a6bc21a2bbfee2cff62dc5a78a8
pfcoder <pfcoder97@gmail.com>
2020-12-25 13:18:31 +0800

    update app type

--- packages/api-derive/src/members/types.ts
+++ packages/api-derive/src/members/types.ts
@@ -1,7 +1,18 @@
 // Copyright 2017-2020 @polkadot/api-derive authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { AccountId } from '@polkadot/types/interfaces';
-import { Vec } from '@polkadot/types';
+import { AccountId, Balance } from '@polkadot/types/interfaces';
+import { Vec, u32 } from '@polkadot/types';
+
+export interface DeriveAppInfo {
+  appId: u32,
+  appName: string,
+  identityAccount: AccountId,
+  adminAccount: AccountId,
+  staking: Balance,
+  returnRate: u32,
+}
+
+export type DeriveAppInfos = DeriveAppInfo[]
 
 export type DeriveRoleAccounts = Vec<AccountId>
